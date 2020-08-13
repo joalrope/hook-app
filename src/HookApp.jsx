428f3994b4a9812028ce734f88962089d07b2754
Jose Rodriguez <joalrope@gmail.com>
2020-08-13 07:14:07 -0400

    first commit

---
+++ src/HookApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function HookApp() {
+  return (
+    <div className="App">
+      <h3>Hook App</h3>  
+    </div>
+  );
+}
+
+export default HookApp;
---
+++ src/components/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+
+import '../../index.css';
+
+export const CounterWithCustomHook = () => {
+    
+    const { state, increment, decrement, reset } = useCounter(100);
+
+
+    return (
+        <>
+            <h1>Counter with hook: { state }</h1>
+            <hr/>
+
+            <button className = 'btn btn-primary' onClick = { () => increment(2) }> +1 </button>
+            <button className = 'btn btn-outline' onClick = { reset }> Reset </button>
+            <button className = 'btn btn-danger'  onClick = { () => decrement(2) }> -1 </button>
+        </>
+    )
+}
+
